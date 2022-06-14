21f35fc9de5e9abe329d368d8d95028a451cc4b1
Jan Bak <jan.pawel.bak01@gmail.com>
2022-06-14 21:19:23 +0200

    basic setup v2

--- src/components/InstaTest.tsx
+++ src/components/InstaTest.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const api = `https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp&access_token={}`;
 
 const InstaTest: React.FC = () => {
-    return <div>InstaTest</div>;
+    useEffect(() => {
+        fetch(
+            `https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp&access_token=${process.env.REACT_APP_FB_TOKEN}`
+        )
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    console.log(result.data);
+                },
+                (error) => {}
+            );
+    }, []);
+
+    return <div>InstaTest {process.env.REACT_APP_FB_TOKEN}</div>;
 };
 
 export default InstaTest;
